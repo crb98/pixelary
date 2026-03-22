b67ad2f8814b46c7dddf78ff4394120b627bda36
crb98 <conniebusby98@gmail.com>
2026-03-22 12:54:38 +0000

    add db initialistation script

--- svr.js
+++ svr.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import Database from 'better-sqlite3';
+import fs from 'fs';
+import path from 'path';
+
+// Initialize database
+const db = new Database('pixelary.db');
+const migration = fs.readFileSync(
+  path.join('migrations-sqlite', '001-initial.sql'),
+  'utf-8'
+);
+db.exec(migration);
 
 const app = express();
 app.use(express.static('client'));
+
+// API for client
+app.get('/api/prompts', (req, res) => {
+  const prompts = db.prepare('SELECT * FROM prompts').all();
+  res.json(prompts);
+});
+
 app.listen(8080, () => {
   console.log('Server running at http://localhost:8080');
 });
