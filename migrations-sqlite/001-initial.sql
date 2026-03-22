67fafff304fb1724fdbddef6a34eedcb278fc175
crb98 <conniebusby98@gmail.com>
2026-03-22 12:50:49 +0000

    create database

---
+++ migrations-sqlite/001-initial.sql
@@ -0,0 +1,15 @@
+CREATE TABLE prompts (
+    id serial PRIMARY KEY,
+    prompt TEXT NOT NULL,
+)
+
+INSERT INTO prompts (prompt) VALUES
+('Hedgehog'),
+('Ice cream cone'),
+('Alien'),
+('Skull'),
+('Pineapple'),
+('Giraffe'),
+('Octopus'),
+('Castle'),
+('Dragon');
